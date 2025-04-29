8d0b010bf511ff9c5939dc95891b21d024b93c25
vitorhenriquec0 <vitorhenriquecardoso59@gmail.com>
2025-04-29 14:37:47 -0300

    Update ex. 9.c

--- Lista 4/ex. 9.c
+++ Lista 4/ex. 9.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 
-int main () {
+int main()
+{ 
+    int a, b, aux;
     
-    int anterior = 0, atual = 1, proximo;
+    a = 0;
+    b = 1;
     
-    printf("Os primeiros 15 termos da sequência de Fibonacci são:\n");
-    
-    // Looping para o cálculo
-    for (int i=1; i<=15; i++) {
-        printf("%d", anterior);
+    for (int i=0; i < 15; i++) {
+        aux = a + b;
+        a = b;
+        b = aux;
         
-        // Adicionar vírgulas, por questões visuais.
-        if (i < 15) {
-            printf(", ");
-        }
-        
-        // Atualizar os termos para a próxima iteração
-        proximo = anterior + atual;
-        
-        anterior = atual; // O termo anterior vira o termo atual
-        atual = proximo; // O "proximo" calculado vira o novo "atual"
-        
+        printf("%d\n", aux);
     }
     
     return 0;
 }
+    
+    
